2c2d875ced0bbac41e7b92e652251e67129d0d94
adwait2007 <74483768+adwait2007@users.noreply.github.com>
2020-12-21 13:19:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var ball;
+var database;
+var position;
+
+var gameState=0;
+var playerCount;
+
+var form,player,game;
+
+function setup(){
+    createCanvas(400,400);
+
+    database=firebase.database();
+    
+    game=new Game();
+    game.getState();
+    game.start();
+
+}
+
+function draw(){
+
+}
+
